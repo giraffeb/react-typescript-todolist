1aec71204b10e8c9bc0f1bba4d134852d7238ff1
giraffeb <rwpakr07@gmail.com>
2019-11-28 19:30:06 +0900

    init

---
+++ src/component/Template/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Top from '../Top';
+
+class Template extends React.Component{
+    render(){
+        return (
+            <>
+                <Top></Top>
+                <h1>Hello new world</h1>
+            </>
+        )
+    }
+
+}
+
+
+export default Template;
---
+++ src/component/Top/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Top: React.FC = ()=>{
+    return (
+        <>
+            <h1>title area</h1>
+        </>
+    )
+}
+
+export default Top;
